097cdfa397b151d426cbc8fc9194ce5ec83193e1
lavan <lavan@192.168.0.72>
2024-03-21 08:50:33 -0500

    Adding a newly created file

---
+++ MultiThreading/src/thread/SingleThread.java
@@ -0,0 +1,10 @@
+package thread;
+
+public class SingleThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adding class with name SingleThread");
+	}
+
+}
